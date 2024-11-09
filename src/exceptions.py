bd0bd126acc0ef280a28384a2ea212b0812ddc2e
Franco Anfossi <franco.anfossi@uc.cl>
2024-11-09 17:21:58 -0300

    feat(exceptions): enhance exception handling with customizable messages and new error types

--- src/exceptions.py
+++ src/exceptions.py
@@ -2,23 +2,26 @@ from fastapi import HTTPException, status
 
 
 class ResourceNotFound(HTTPException):
-    def __init__(self, resource: str):
+    def __init__(self, resource: str, message: str = None):
+        detail = message if message else f"{resource} not found."
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
+
+
+class ResourceConflict(HTTPException):
+    def __init__(self, resource: str, message: str = None):
+        detail = message if message else f"{resource} already exists."
+        super().__init__(status_code=status.HTTP_409_CONFLICT, detail=detail)
+
+
+class InvalidDataError(HTTPException):
+    def __init__(self, message: str = "Invalid data provided."):
         super().__init__(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"{resource} not found."
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=message
         )
 
 
-class InvalidCredentials(HTTPException):
-    def __init__(self):
+class DatabaseError(HTTPException):
+    def __init__(self, message: str = "A database error occurred."):
         super().__init__(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid credentials.",
-            headers={"WWW-Authenticate": "Bearer"},
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=message
         )
-
-
-class PermissionDenied(HTTPException):
-    def __init__(
-        self, message: str = "You do not have permission to perform this action."
-    ):
-        super().__init__(status_code=status.HTTP_403_FORBIDDEN, detail=message)
